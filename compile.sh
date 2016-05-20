99e155c81cacc568414ca498aa8d1058973e2b6e
Lia Corrales <lia.astro@gmail.com>
2016-05-20 12:19:26 -0400

    Will no longer create lmodel.dat file

--- compile.sh
+++ compile.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-ln -sf lmodel_ismdust.dat lmodel.dat
-
-echo "initpackage ismdust lmodel.dat "`pwd`"\nquit\ny" | xspec
+echo "initpackage ismdust lmodel_ismdust.dat "`pwd`"\nquit\ny" | xspec
 
 rm *~ *.o
 rm *FunctionMap.* lpack_*
